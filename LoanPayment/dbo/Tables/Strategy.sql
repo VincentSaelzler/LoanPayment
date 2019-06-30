14ffcf72b63485e6cd3cbc2daa2e26f1e3052b85
Vincent Saelzler <vincent.saelzler@gmail.com>
2019-06-30 11:24:56 -0400

    fix typo: strategy PK name

--- LoanPayment/dbo/Tables/Strategy.sql
+++ LoanPayment/dbo/Tables/Strategy.sql
@@ -6,6 +6,8 @@
     [StrategyId]              INT            NOT NULL,
     [StrategyName]            NVARCHAR (255) NOT NULL,
     [UseSortOrderGroup]       NVARCHAR (255) NOT NULL,
-    CONSTRAINT [PK_Table] PRIMARY KEY CLUSTERED ([StrategyId] ASC)
+    CONSTRAINT [PK_Strategy] PRIMARY KEY CLUSTERED ([StrategyId] ASC)
 );
+
+
 
