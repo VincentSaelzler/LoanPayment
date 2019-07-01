37198ed3c60c8c10d84bea8b5ff352160fdcfa32
Vincent Saelzler <vincent.saelzler@gmail.com>
2019-06-30 22:56:58 -0400

    fix typo: correct PK for payment table

--- LoanPayment/dbo/Tables/Payment.sql
+++ LoanPayment/dbo/Tables/Payment.sql
@@ -8,9 +8,11 @@
     [Principal]           FLOAT (53) NOT NULL,
     [PrincipalBalance]    FLOAT (53) NOT NULL,
     [StrategyId]          INT        NOT NULL,
-    CONSTRAINT [PK_Payment] PRIMARY KEY CLUSTERED ([StrategyId] ASC),
+    CONSTRAINT [PK_Payment] PRIMARY KEY CLUSTERED ([PaymentId] ASC),
     CONSTRAINT [FK_Payment_Loan] FOREIGN KEY ([LoanId]) REFERENCES [dbo].[Loan] ([LoanId]),
     CONSTRAINT [FK_Payment_Month] FOREIGN KEY ([MonthId]) REFERENCES [dbo].[Month] ([MonthId]),
     CONSTRAINT [FK_Payment_Strategy] FOREIGN KEY ([StrategyId]) REFERENCES [dbo].[Strategy] ([StrategyId])
 );
+
+
 
