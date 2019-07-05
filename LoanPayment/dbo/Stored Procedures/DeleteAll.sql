e8ddeb3a4f5335ccd0d68abf413cff5bbd754c2f
Vincent Saelzler <vincent.saelzler@gmail.com>
2019-07-05 12:51:12 -0400

    add delete all SPROC

---
+++ LoanPayment/dbo/Stored Procedures/DeleteAll.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[DeleteAll]
+AS
+	--fact table can be truncated
+	TRUNCATE TABLE dbo.Payment;
+
+	--dimensions are referenced by FK, so must use DELETE
+	DELETE FROM dbo.Loan;
+	DELETE FROM dbo.[Month];
+	DELETE FROM dbo.Strategy;
+RETURN 0
