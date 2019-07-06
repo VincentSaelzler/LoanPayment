ef2b6fbb7e669a2995a75c8a90c7abab28a7563f
Vincent Saelzler <vincent.saelzler@gmail.com>
2019-07-06 18:43:03 -0400

    add DeleteAllExceptLoan

---
+++ LoanPayment/dbo/Stored Procedures/DeleteAllExceptLoan.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[DeleteAllExceptLoan]
+AS
+	--this SPROC exists because the web UI of the program associated with this DB
+	--lets people create/edit/delete their own loan records.
+	--everything else is auto-calculated.
+
+	--fact table can be truncated
+	TRUNCATE TABLE dbo.Payment;
+
+	--dimensions are referenced by FK, so must use DELETE
+	--DELETE FROM dbo.Loan;
+	DELETE FROM dbo.[Month];
+	DELETE FROM dbo.Strategy;
+RETURN 0
